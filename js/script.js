1c555b7a803c7067ed1c24007d0d6e851a308015
pasindu p konghawaththa <37585474+MasterBrian99@users.noreply.github.com>
2019-12-23 00:17:59 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function showsingin() {
+   document.getElementById('contain').style.display = "block";
+   document.getElementById("contain_signup").style.display = "none";
+     document.getElementById("headername").style.display = "none";
+}
+
+function showsingup() {
+   document.getElementById('contain_signup').style.display = "block";
+    document.getElementById("contain").style.display = "none";
+         document.getElementById("headername").style.display = "none";
+}
